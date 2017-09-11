70dd91d8b791f6655fb61ab6c67c1cb11e8b3469
naseef012 <naseefmohammed012@gmail.com>
2017-09-11 19:48:40 +0600

    Modified the PageController which now returns "page.jsp" as the view

--- src/main/java/com/naseef/onlineshopping/controller/PageController.java
+++ src/main/java/com/naseef/onlineshopping/controller/PageController.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Created by USER on 8/25/2017.
+ * Created by USER on 9/11/2017.
  */
 @Controller
 public class PageController
 {
-    @RequestMapping (value = {"/", "/home", "/index"})
-    public ModelAndView index()
+    @RequestMapping (value = {"/", "/index","/home"})
+    public ModelAndView index ()
     {
         ModelAndView mv = new ModelAndView("page");
-        mv.addObject("greetung" , "Welcome To Spring Project");
+        mv.addObject("greeting" , "Welcome to Spring MVC");
         return mv;
     }
 }
